550abce6790eb32450048dec6f6aa916722f437c
Scott Moschella <scottmosch@gmail.com>
2011-04-08 00:26:46 -0700

    Finished password logic for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
+require 'digest'
+
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
-  
-  before_save :encrypt_password
-  
+    
   validates(:name, :length => {:maximum => 50})
   validates(:email, :presence => true,
                     :uniqueness => {:case_sensitive => false})
@@ -11,18 +11,36 @@ class User < ActiveRecord::Base
                       :confirmation => true,
                       :length => {:within => 6..40})
   
+  before_save :encrypt_password
+  
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
   
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  
   private
   
   def encrypt_password
+    self.salt = make_salt if new_record?
     self.encrypted_password = encrypt(password)
   end
   
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+  
   def encrypt(string)
-    string
+    secure_hash("#{salt}--#{string}")
+  end
+  
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
   end
   
 end
