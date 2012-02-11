28b23923b5738e6169236b5c5322c7cb4fca4bae
Scott Moschella <scottmosch@gmail.com>
2012-02-11 01:14:19 -0800

    Added Gmail settings to mailer

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com'
+  :user_name            => 'scottmosch@gmail.com',
+  :password             => 'LosAngeles&m3',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
