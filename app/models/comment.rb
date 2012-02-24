2cd0e621283c863171ed19d606fe99ee3b722390
Scott Moschella <scottmosch@gmail.com>
2012-02-24 00:42:36 -0800

    Readded validations to comments after import

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :message
-#  belongs_to :user
+  belongs_to :user
 
-  #validates_presence_of :content, :author, :author_email
-  #validates_length_of :content,
-  #                    :minimum => 1,
-  #                    :maximum => 3000
+  validates_presence_of :content, :author, :author_email
+  validates_length_of :content,
+                      :minimum => 1,
+                      :maximum => 3000
 end
